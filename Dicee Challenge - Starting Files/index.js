f9f80e9dfb78c05c9641c123bb0fa5f916316cdb
Yaniv Mendiuk <yaniv199666@gmail.com>
2020-03-21 16:49:55 +0200

    Declering the winner

--- Dicee Challenge - Starting Files/index.js
+++ Dicee Challenge - Starting Files/index.js
@@ -1,8 +1,6 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-<<<<<<< HEAD
 console.log(randomNumber1, randomNumber2);
-=======
 console.log;
 document
   .querySelector(".img1")
@@ -10,4 +8,11 @@ document
 document
   .querySelector(".img2")
   .setAttribute("src", "images/dice" + randomNumber2 + ".png");
->>>>>>> Set
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 1 wins";
+} else if (randomNumber1 < randomNumber2) {
+  document.querySelector("h1").innerHTML = "Player 2 wins";
+} else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
